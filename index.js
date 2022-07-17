da9450f3d68ff4192d676c1d994b872fd0603b87
Uniqueoduwa <102852363+Uniqueoduwa@users.noreply.github.com>
2022-07-17 10:38:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("welcome");
+var name = prompt(" FILL IN!  . what is your name?");
+
+ document.write (" <h1> hi  " + name +"! <br> <br> please go and learn work <br> <br> Dem nor dey pluck money from tree</h1>" );
+ console.log(name);
